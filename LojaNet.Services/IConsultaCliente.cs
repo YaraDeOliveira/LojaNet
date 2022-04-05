d3957608eebbcc8b3dc29b9f9e0f1b50dc85f416
Yara <yara.regina@hotmail.com>
2022-04-04 21:42:19 -0300

    Criando serviço WCF

---
+++ LojaNet.Services/IConsultaCliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using LojaNetF.Models;
+
+namespace LojaNet.Services {
+   
+    [ServiceContract]
+    public interface IConsultaCliente {
+        [OperationContract]
+        ClienteInfo ConsultarPorEmail(string chave, string email);
+
+    }
+}
