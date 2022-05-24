e6515f3e062bfbf3d0b151a267b7bb245cfc8f48
Yara Regina <yara.regina@hotmail.com>
2022-05-24 20:52:40 -0300

    Começando a autenticação por cookies

---
+++ LojaNetFwk.Ui.Web/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+
+[assembly: OwinStartup(typeof(LojaNetFwk.Ui.Web.Startup))]
+
+namespace LojaNetFwk.Ui.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            { 
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/usuario/login")
+            }
+            );
+               
+        }
+    }
+}
