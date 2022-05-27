592043278ab157e228d109cd086af162949eacc9
Yara Regina <yara.regina@hotmail.com>
2022-05-27 20:19:53 -0300

    Criando a View Model

---
+++ LojaNetFwk.Ui.Web/Models/PedidoViewModel.cs
@@ -0,0 +1,43 @@
+﻿using LojaNetF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LojaNetFwk.Ui.Web.Models
+{
+    public class PedidoViewModel
+    {
+        public PedidoViewModel()
+        {
+            this.Clientes = new List<Cliente>();
+            this.Produtos = new List<Produto>();
+            this.Items = new List<Item>();
+            this.FormasPagamento = new List<string>();
+            this.Data = DateTime.Now;
+        }
+
+        public string Id { get; set; }
+        public DateTime Data { get; set; }
+        public List<Cliente> Clientes { get; set; }
+        public string ClienteId { get; set; }
+        public List<Item> Items { get; set; }
+        public FormaPagtoEnum FormaPagamento { get; set; }
+        public List<string> FormasPagamento { get; set; }
+        public List<Produto> Produtos { get; set; }
+
+
+        public class Item
+        {
+            public string ProdutoId { get; set; }
+            public string ProdutoNome { get; set; }
+            public int Quantidade { get; set; }
+            public decimal Valor { get; set; }
+            public decimal Total
+            {
+                get { return Valor * Quantidade; }
+
+            }
+        }
+    }
+}
