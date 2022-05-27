f615dae27e06cedee476b3d4cedc7e3512adb521
Yara Regina <yara.regina@hotmail.com>
2022-05-27 20:42:46 -0300

    Tela de Inclusao de Pedidos

---
+++ LojaNetFwk.Ui.Web/Controllers/PedidoController.cs
@@ -0,0 +1,23 @@
+﻿using LojaNetFwk.Ui.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaNetFwk.Ui.Web.Controllers
+{
+    public class PedidoController : Controller
+    {
+        // GET: Pedido
+        public ActionResult Incluir()
+        {
+            var bllCliente = AppContainer.ObterClienteBLL();
+            
+            var pedido = new PedidoViewModel();
+            pedido.Clientes = bllCliente.ObterTodos();
+            
+            return View(pedido);
+        }
+    }
+}
