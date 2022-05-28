65e795381f5eeb01c8482d33b5ad26742049e0c4
Yara Regina <yara.regina@hotmail.com>
2022-05-27 21:33:14 -0300

    Details

--- LojaNetFwk.Ui.Web/Controllers/PedidoController.cs
+++ LojaNetFwk.Ui.Web/Controllers/PedidoController.cs
@@ -18,6 +18,30 @@ namespace LojaNetFwk.Ui.Web.Controllers
             var pedido = new PedidoViewModel();
             pedido.Clientes = bllCliente.ObterTodos();
             pedido.FormasPagamento = Enum.GetNames(typeof(FormaPagtoEnum)).ToList();
+            pedido.Items.Add(new PedidoViewModel.Item()
+            {
+                ProdutoId = "837483",
+                ProdutoNome = "Caneta",
+                Quantidade = 10,
+                Valor = 2
+            });
+
+            pedido.Items.Add(new PedidoViewModel.Item()
+            {
+                ProdutoId = "7787857",
+                ProdutoNome = "Borracha",
+                Quantidade = 180,
+                Valor = 100
+            });
+
+            pedido.Items.Add(new PedidoViewModel.Item()
+            {
+                ProdutoId = "998367",
+                ProdutoNome = "Lápis",
+                Quantidade = 150,
+                Valor = 320
+            });
+
             return View(pedido);
         }
     }
