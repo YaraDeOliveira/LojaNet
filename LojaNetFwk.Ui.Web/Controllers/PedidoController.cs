61def79dde4057d203e905d827b2c45e57442de0
Yara Regina <yara.regina@hotmail.com>
2022-05-27 21:03:59 -0300

    Exibindo Enumeradores

--- LojaNetFwk.Ui.Web/Controllers/PedidoController.cs
+++ LojaNetFwk.Ui.Web/Controllers/PedidoController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LojaNetF.Models;
 
 namespace LojaNetFwk.Ui.Web.Controllers
 {
@@ -16,7 +17,7 @@ namespace LojaNetFwk.Ui.Web.Controllers
             
             var pedido = new PedidoViewModel();
             pedido.Clientes = bllCliente.ObterTodos();
-            
+            pedido.FormasPagamento = Enum.GetNames(typeof(FormaPagtoEnum)).ToList();
             return View(pedido);
         }
     }
