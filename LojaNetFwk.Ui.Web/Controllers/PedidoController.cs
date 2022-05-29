2680c53b896de1b1329f9384bb63d30f3ecfb457
Yara Regina <yara.regina@hotmail.com>
2022-05-29 17:31:13 -0300

    Manter as listas de Opcoes

--- LojaNetFwk.Ui.Web/Controllers/PedidoController.cs
+++ LojaNetFwk.Ui.Web/Controllers/PedidoController.cs
@@ -16,55 +16,70 @@ namespace LojaNetFwk.Ui.Web.Controllers
         {
             var bllCliente = AppContainer.ObterClienteBLL();
             var bllProduto = AppContainer.ObterProdutoBLL();
-            
+
             var pedido = new PedidoViewModel();
+
             pedido.Clientes = bllCliente.ObterTodos();
             pedido.Produtos = bllProduto.ObterTodos();
             pedido.FormasPagamento = Enum.GetNames(typeof(FormaPagtoEnum)).ToList();
-            pedido.Items.Add(new PedidoViewModel.Item()
-            {
-                ProdutoId = "837483",
-                ProdutoNome = "Caneta",
-                Quantidade = 10,
-                Valor = 2
-            });
 
-            pedido.Items.Add(new PedidoViewModel.Item()
-            {
-                ProdutoId = "7787857",
-                ProdutoNome = "Borracha",
-                Quantidade = 180,
-                Valor = 100
-            });
+            //pedido.Items.Add(new PedidoViewModel.Item()
+            //{
+            //    ProdutoId = "837483",
+            //    ProdutoNome = "Caneta",
+            //    Quantidade = 10,
+            //    Valor = 2
+            //});
 
-            pedido.Items.Add(new PedidoViewModel.Item()
-            {
-                ProdutoId = "998367",
-                ProdutoNome = "Lápis",
-                Quantidade = 150,
-                Valor = 320
-            });
+            //pedido.Items.Add(new PedidoViewModel.Item()
+            //{
+            //    ProdutoId = "7787857",
+            //    ProdutoNome = "Borracha",
+            //    Quantidade = 180,
+            //    Valor = 100
+            //});
+
+            //pedido.Items.Add(new PedidoViewModel.Item()
+            //{
+            //    ProdutoId = "998367",
+            //    ProdutoNome = "Lápis",
+            //    Quantidade = 150,
+            //    Valor = 320
+            //});
 
             return View(pedido);
         }
 
-        [HttpPost]  
+        [HttpPost]
         public ActionResult Incluir(PedidoViewModel pedido)
         {
+            var bllProduto = AppContainer.ObterProdutoBLL();
+            var bllCliente = AppContainer.ObterClienteBLL();
+
+            pedido.Clientes = bllCliente.ObterTodos();
+            pedido.Produtos = bllProduto.ObterTodos();
+            pedido.FormasPagamento = Enum.GetNames(typeof(FormaPagtoEnum)).ToList();
 
             if (Request.Form["incluirProduto"] == "Incluir")
             {
+
                 var item = new PedidoViewModel.Item();
                 item.ProdutoId = pedido.NovoItemProdutoId;
                 item.Quantidade = pedido.NovoItemQuantidade;
 
-                var bllProduto = AppContainer.ObterProdutoBLL();
+
                 var produto = bllProduto.ObterPorId(item.ProdutoId);
                 item.Valor = produto.Preco;
                 item.ProdutoNome = produto.Nome;
+
                 pedido.Items.Add(item);
 
             }
+            else if (Request.Form["Gravar"] == "Gravar")
+            {
+                //var bll = AppContainer.ObterPedidoBLL();
+            }
+
             return View(pedido);
         }
     }
